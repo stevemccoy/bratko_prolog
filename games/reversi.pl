23d3ec56bb6d94a6b22f7e79c399696c86f90225
Steve McCoy <stephen.mccoy@bjss.com>
2019-03-18 13:11:21 +0000

    Improved indexed item retrieval, etc.

--- games/reversi.pl
+++ games/reversi.pl
@@ -1,5 +1,5 @@
 
-% Reversi Game
+% Reversi (Othello) Game
 
 % Start position 2 white and 2 black stones.
 % Take turns, black first.
@@ -9,8 +9,19 @@
 % Game position representation:
 % Grid of elements from (b,w,s)
 
-% Row and column coordinates from the following set:
-coordinates([1,2,3,4,5,6,7,8,9]).
+
+row_coordinate(R) :-
+	member(R, [1,2,3,4,5,6,7,8]).
+
+
+column_coordinate(C) :-
+	member(C, [1,2,3,4,5,6,7,8]).
+
+
+valid_coordinate(R/C) :-
+	row_coordinate(R),
+	column_coordinate(C).
+
 
 position([
 	[s,s,s,s,s,s,s,s],
@@ -25,26 +36,35 @@ position([
 
 % Get and set indexed positions in a list.
 
-get_indexed_item(1, [V | _], V).
-get_indexed_item(N, [_ | T], V) :-
-	N > 1,
-	M is N - 1,
-	get_indexed_item(M, T, V).
-
 set_indexed_item(1, [_ | Tail], V, [V | Tail]).
 set_indexed_item(N, [Head | Tail], V, [Head | After]) :-
 	N > 1,
 	M is N - 1,
 	set_indexed_item(M, Tail, V, After).
 
+
+% Get value at an indexed position, or find the indexes of matches to the provided value, within a given list.
+% List must be a finite list - procedure cannot be used to generate List.
+
+indexed_item(Value, List, Index) :-
+	not(var(List)),
+	find_index_with_offset(Value, List, 0, Index).
+
+find_index_with_offset(Value, [Value | _], Offset, N) :-
+	N is Offset + 1.
+find_index_with_offset(Value, [_ | Tail], Offset, N) :-
+	NewOffset is Offset + 1,
+	find_index_with_offset(Value, Tail, NewOffset, N).
+
+
 % Get and set the value of a given square.
 
 get_square(R/C, Grid, Value) :-
-	get_indexed_item(R, Grid, Row),
-	get_indexed_item(C, Row, Value).
+	indexed_item(Row, Grid, R),
+	indexed_item(Value, Row, C).
 
 set_square(R/C, Before, V, After) :-
-	get_indexed_item(R, Before, Row),
+	indexed_item(Row, Before, R),
 	set_indexed_item(C, Row, V, NewRow),
 	set_indexed_item(R, Before, NewRow, After).
 
@@ -58,18 +78,25 @@ set_square(R/C, Before, V, After) :-
 
 % Find out squares converted by a single move.
 
-beam_directions([-1/-1, -1/0, -1/1, 0/-1, 0/1, 1/-1, 1/0, 1/1]).
+direction(-1, -1).
+direction(-1, 0).
+direction(-1, 1).
+direction(0, -1).
+direction(0, 1).
+direction(1, -1).
+direction(1, 0).
+direction(1, 1).
+
+% beam_directions([-1/-1, -1/0, -1/1, 0/-1, 0/1, 1/-1, 1/0, 1/1]).
 
 
 convert_all_beams(Position, StartRow/StartCol, Them, Us, TurnOverList) :-
-	beam_directions(Directions),
-	!,
 	findall(BeamList, (
-		member(DR/DC, Directions), 
+		direction(DR, DC),
 		R is StartRow + DR,
 		C is StartCol + DC,
 		valid_coordinate(R/C),
-		convert_beam(Position, R/C, Direction, Them, Us, BeamList)
+		convert_beam(Position, R/C, DR/DC, Them, Us, BeamList)
 	), BeamLists),
 	flatten(BeamLists, TurnOverList).
 
@@ -79,7 +106,7 @@ convert_all_beams(Position, StartRow/StartCol, Them, Us, TurnOverList) :-
 % Position, Start, Direction, TheirColour, OurColour, TurnOverList
 % convert_beam(Position, StartRow/StartCol, DRow/DCol, Them/Us, TurnOverList) :-
 
-convert_beam(Position, SRow/SCol, DRow/DCol, Them/Us, []) :-
+convert_beam(Position, SRow/SCol, _/_, _/Us, []) :-
 	get_square(SRow/SCol, Position, Us),
 	!.
 
@@ -98,6 +125,7 @@ make_move(Position1, Row/Col, Us, TurnOverList, Position3) :-
 	set_square(Row/Col, Position1, Us, Position2),
 	set_all_squares(TurnOverList, Position2, Us, Position3).
 
+
 set_all_squares([], Position, _, Position).
 set_all_squares([R/C | Tail], Position1, Us, Position3) :-
 	set_square(R/C, Position1, Us, Position2),
@@ -109,34 +137,24 @@ set_all_squares([R/C | Tail], Position1, Us, Position3) :-
 % Generate adjacent coordinates to R1/C1.
 
 adjacent(R1/C1, R2/C2) :-
-	beam_directions(Directions),
-	!,
-	member(DR/DC, Directions),
+	direction(DR, DC),
 	R2 is R1 + DR,
 	C2 is C1 + DC.
 
 
 % Find squares with given value.
 
+/*
 find_squares_with(Position, Value, Results) :-
-	setof(R/C, (), Results)
+	setof(R/C, (), Results).
+*/
 
 % Need to scan through the rows in Position, keeping track of index positions, to capture matching entries.
 % find_index_of(Value, List, Index)
 
 % indexed_content(List, Index, Value)
 
-indexed_content([H | Tail], 1, H).
-indexed_content([_ | Tail], N, H) :-
-	inde
-
-
-
-find_index_of(Value, [Value | Tail], 1).
-find_index_of(Value, [_ | Tail], N) :-
-	find_index_of
-
-
+/*
 
 
 % move(Position1, Us/Them, R/C, Position2)
@@ -147,3 +165,4 @@ possible_move(Position, Us/Them, R/C) :-
 	% Non-empty conversion list.
 
 
+*/
