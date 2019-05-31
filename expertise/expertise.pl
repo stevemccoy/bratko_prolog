188c5314a49dfec0c798be3a1c0362ffacdd54bd
Steve McCoy <stephen.mccoy@bjss.com>
2019-05-31 16:33:28 +0100

    Shaping up logic for expertise search

--- expertise/expertise.pl
+++ expertise/expertise.pl
@@ -6,3 +6,31 @@
 
 % skill(Category, SkillName, [Alias, ... ], ParentSkill).
 
+
+% Match name strings. Initially just do a case invariant string comparison.
+
+matching_name(Name1, Name2) :-
+	string_lower(Name1, LowerName),
+	string_lower(Name2, LowerName).
+
+
+% matching_skill_under(Parent, Name, MatchList)
+
+matching_skill_under(Parent, Name, MatchList) :-
+	setof(Match, (
+		skill(Parent, Match),
+		matching_name(Name, Match)
+	), MatchList).
+
+
+% find_skill(SkillName, Horizon, UpList, MatchList, DownList)
+
+
+
+find_skill(Name, 0, [], Match, []) :-
+	matching_skill_under(_, Name, Match).
+
+find_skills(Name, Horizon, Up, Match, Down) :-
+	Horizon > 0,
+	
+
