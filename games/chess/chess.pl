3061f068688a34ab574cfc7114894b9212e1c384
SteveMcCoy <steve_mccoy@hotmail.co.uk>
2023-05-13 11:32:26 +0100

    Added chess for possible workshop

---
+++ games/chess/chess.pl
@@ -0,0 +1,17 @@
+/*
+Prolog Workshop
+
+Idea: Chess problem solver (mate in N moves).
+
+1. Board state representation.
+2. Move definition.
+2a. Deciding if position is checkmate or not.
+3. Turn taking.
+4. Generate possible moves from a board position.
+5. Search for mate for Colour in Num moves from position.
+
+6. (optional) Board state evaluation - scoring heuristic.
+7. (optional) Minimax search tree pruning.
+
+*/
+
